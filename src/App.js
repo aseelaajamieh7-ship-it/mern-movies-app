3510df21cb049fb42f1b1b8f5cebb9e12138208b
Aseel <aseelaajamieh7@gmail.com>
2026-05-20 03:20:10 +0300

    frontend render update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,115 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Movies from "./pages/Movies";
+import About from "./pages/About";
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <BrowserRouter>
+
+      <div
+        style={{
+          backgroundColor: "#222",
+          padding: "20px",
+        }}
+      >
+
+        <Link
+          to="/"
+          style={{
+            color: "white",
+            marginRight: "20px",
+            textDecoration: "none",
+            fontSize: "18px",
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          Home
+        </Link>
+
+        <Link
+          to="/register"
+          style={{
+            color: "white",
+            marginRight: "20px",
+            textDecoration: "none",
+            fontSize: "18px",
+          }}
+        >
+          Register
+        </Link>
+
+        <Link
+          to="/login"
+          style={{
+            color: "white",
+            marginRight: "20px",
+            textDecoration: "none",
+            fontSize: "18px",
+          }}
+        >
+          Login
+        </Link>
+
+        <Link
+          to="/movies"
+          style={{
+            color: "white",
+            marginRight: "20px",
+            textDecoration: "none",
+            fontSize: "18px",
+          }}
+        >
+          Movies
+        </Link>
+
+        <Link
+          to="/about"
+          style={{
+            color: "white",
+            textDecoration: "none",
+            fontSize: "18px",
+          }}
+        >
+          About
+        </Link>
+
+      </div>
+
+
+      <Routes>
+
+        <Route
+          path="/"
+          element={<Home />}
+        />
+
+        <Route
+          path="/register"
+          element={<Register />}
+        />
+
+        <Route
+          path="/login"
+          element={<Login />}
+        />
+
+        <Route
+          path="/movies"
+          element={<Movies />}
+        />
+
+        <Route
+          path="/about"
+          element={<About />}
+        />
+
+      </Routes>
+
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/About.js
@@ -0,0 +1,20 @@
+function About() {
+
+  return (
+    <div
+      style={{
+        textAlign: "center",
+        marginTop: "100px",
+        fontFamily: "Arial",
+      }}
+    >
+      <h1>About</h1>
+
+      <p>
+        This Movie App was built using MERN Stack.
+      </p>
+    </div>
+  );
+}
+
+export default About;
